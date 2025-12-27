d02aea69be83c23a36c53f1641916cee0166b3a5
Sujit Walmiki <sujitwalmiki.nbnssoe.comp@gmail.com>
2025-12-28 02:01:27 +0530

    More Files

---
+++ hostel-management/hostel-management/src/main/java/com/hostelmanagement/system/HostelManagementApplication.java
@@ -0,0 +1,14 @@
+package com.hostelmanagement.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelManagementApplication.class, args);
+        System.out.println("Hello");
+	}
+
+}
---
+++ hostel-management/hostel-management/src/test/java/com/hostelmanagement/system/HostelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostelmanagement.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
